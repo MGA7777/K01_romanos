3f3e81cbda4f82d0b0eb953b67ee88a34eec5c2c
Marta Gomez <mgomez@gdma.es>
2023-07-24 22:42:08 +0200

    feat: Validacion entre 1 y 3999

--- Kata01_romanos.py
+++ Kata01_romanos.py
@@ -1,3 +1,19 @@
 def convertir_a_romano(numero):
     if type(numero) != int:
-        return f"Error: Debes introducir un númeero entero"
+        return f"Error: Debes introducir un número entero"
+    if not 0 < numero < 4000:
+        return f"Error: El número debe estar entre 0 y 3999 ({numero})"
+    return "TODO: Convertir a romano"
+
+
+# if not isinstance(numero, int):
+#   return "Error: Debe introducir un número entero ({numero})"  
+# return "TODO: Convrtir a romano"
+
+print (convertir_a_romano (56))
+print (convertir_a_romano (0))
+print (convertir_a_romano (4000))
+print (convertir_a_romano (-7))
+print (convertir_a_romano ("doce"))
+print (convertir_a_romano (1))
+print (convertir_a_romano (3999))
