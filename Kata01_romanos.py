e599c97ff47af9de682f248fa9a9d92bf205aff7
MGA7777 <mgomez@gdma.es>
2023-07-25 22:07:45 +0200

    Feat: Funciona restando si número anterior en menor que el siguiente

--- Kata01_romanos.py
+++ Kata01_romanos.py
@@ -28,21 +28,45 @@ print ("El numero introducido equivale a ", num_rom_2, "en romano.")
 
 def romano_a_entero(romano):
 
-    digitos_romanos = ["I", "V", "X", "L", "C", "D", "M"]
+    """digitos_romanos = ["I", "V", "X", "L", "C", "D", "M"]
+    valores = [1, 5, 10, 50, 100, 500, 1000]"""
+    digitos_romanos = {
+        "I": 1, 
+        "V": 5, 
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
 
     if not isinstance (romano, str):
         return "Error: Tiene que ser un número romano en formato cadena de texto"
+    
+    resultado = 0
+    anterior = 0
 
     for letra in romano:
         if letra not in digitos_romanos:
             return f"Error: {letra} no es un dígito romano válido (I, V, X, L, C, D, M)"
-    
-    return "Todo: devolver el resultado"
+        #i = digitos_romanos.index(letra)
+        #resultado = resultado + valores [i]
+        #resultado = resultado + digitos_romanos[letra]
+        actual = digitos_romanos [letra]
+        
+        if anterior < actual:
+            #deshace la suma de la anterior condicion
+            resultado = resultado - anterior
+            resultado = resultado + (actual - anterior)
+        else:
+            resultado = resultado + actual
+        anterior = actual
+        
+    return resultado
 
-pruebas = ["A", "", 3, ["X", "X", "I"], "I", "MCXXIII"]
+pruebas = ["A", "", 3, ["X", "X", "I"], "I", "MCXXIII", "IV", "XIV", "XM"]
 for valor in pruebas:
     print(romano_a_entero(valor))
-
 
 
 
